15e3da51f0a01ec4b494a370e6ac204850e0da59
Hesam Damghanian <hdamghanian77@gmail.com>
2021-03-29 23:51:43 +0430

    first commit

---
+++ driver.py
@@ -0,0 +1,42 @@
+import sys
+import lexer
+
+
+if len(sys.argv) > 1:
+    data = open(sys.argv[1], 'rb').read().decode('utf-8')
+else:
+    data = \
+    """
+    void main()
+    {
+        int k = 10;
+        int i;
+        for(i = 0; i < 10; i++)
+        {
+            if (i >= 5)
+            {
+                k += i;
+            }
+        }
+
+        printstr("\nHello,")  /* forgot ;*/
+        printstr(" World!");
+
+        return;
+    }
+    """
+
+# Build the lexer
+lexer = lex.lex()
+
+# Give the lexer some input
+lexer.input(data)
+
+
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok.type, tok.value, tok.lineno, tok.lexpos)
---
+++ test.py
@@ -0,0 +1,113 @@
+import ply.lex as lex
+
+# List of token names.   This is always required
+tokens = (
+   'NUMBER',
+   'PLUS',
+   'MINUS',
+   'TIMES',
+   'DIVIDE',
+   'LPAREN',
+   'RPAREN',
+)
+
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+
+# A regular expression rule with some action code
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+
+# Test it out
+data = '3 + 4 * 10 + -2%0 *2'
+
+# Give the lexer some input
+lexer.input(data)
+
+# Tokenize
+while True:
+    tok = lexer.token()
+    if not tok:
+        break      # No more input
+    print(tok.type, tok.value, tok.lineno, tok.lexpos)
+
+# Yacc example
+
+import ply.yacc as yacc
+
+# Get the token map from the lexer.  This is required.
+# from calclex import tokens
+
+def p_expression_plus(p):
+    'expression : expression PLUS term'
+    p[0] = p[1] + p[3]
+
+def p_expression_minus(p):
+    'expression : expression MINUS term'
+    p[0] = p[1] - p[3]
+
+def p_expression_term(p):
+    'expression : term'
+    p[0] = p[1]
+
+def p_term_times(p):
+    'term : term TIMES factor'
+    p[0] = p[1] * p[3]
+
+def p_term_div(p):
+    'term : term DIVIDE factor'
+    p[0] = p[1] / p[3]
+
+def p_term_factor(p):
+    'term : factor'
+    p[0] = p[1]
+
+def p_factor_num(p):
+    'factor : NUMBER'
+    p[0] = p[1]
+
+def p_factor_expr(p):
+    'factor : LPAREN expression RPAREN'
+    p[0] = p[2]
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
+# Build the parser
+yacc.yacc()
+
+# Use this if you want to build the parser using SLR instead of LALR
+# yacc.yacc(method="SLR")
+
+while 1:
+   try:
+       s = raw_input('calc > ')
+   except EOFError:
+       break
+   if not s: continue
+   result = yacc.parse(s)
+   print(result)
