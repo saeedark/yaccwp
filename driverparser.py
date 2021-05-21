859d73c73360c8a08d0583a85592bb6c7e64966e
saeed <saeed.ali.rahimi@gmail.com>
2021-05-22 00:10:54 +0430

    tree tada

--- driverparser.py
+++ driverparser.py
@@ -27,14 +27,34 @@ else:
 commands = yacc.parse(data)
 
 
-def tupleprint(s, x):
-    if isinstance(s, tuple):
-        for i in range(len(s)):
-            tupleprint(s[i], x+3)
-    else:
-        print("  "*x + str(s))
 
-tupleprint(commands, 0)
+def build_tree(root):
+    return '\n'.join(_build_tree(root))
 
+
+def _build_tree(node):
+    if not isinstance(node, tuple):
+        yield str(node)
+        return
+
+    values = [_build_tree(n) for n in node]
+    if len(values) == 1:
+        yield from build_lines('──', '  ', values[0])
+        return
+
+    start, *mid, end = values
+    yield from build_lines('┬─', '│ ', start)
+    for value in mid:
+        yield from build_lines('├─', '│ ', value)
+    yield from build_lines('└─', '  ', end)
+
+
+def build_lines(first, other, values):
+    yield first + next(values)
+    for value in values:
+        yield other + value
+
+
+print(build_tree(commands))
 #for i in range(len(commands)):
    # print(commands[i])
